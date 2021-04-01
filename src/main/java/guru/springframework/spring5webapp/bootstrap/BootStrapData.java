5acd9a2647296f480b7425d1de84ba92daab481d
Callum <callumoakey@hotmail.com>
2021-04-01 11:42:53 +0200

    Assnmnt add publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -24,30 +24,34 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher calPub = new Publisher("rue Washington 10", "Callum Publishing House");
+
+        publisherRepository.save(calPub);
 
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain driven design", "123123");
+        Book ddd = new Book("Domain driven design", "123123", calPub);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        calPub.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(calPub);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "132132");
+        Book noEJB = new Book("J2EE Development without EJB", "132132", calPub);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        calPub.getBooks().add(noEJB);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher calPub = new Publisher("rue Washington 10", "Callum Publishing House");
-
         publisherRepository.save(calPub);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Publisher number of Books: " + calPub.getBooks().size());
 
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -14,6 +14,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -22,9 +25,10 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
+        this.publisher = publisher;
     }
 
     public Long getId() {
@@ -82,5 +86,13 @@ public class Book {
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 }
